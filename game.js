61f7a8df3e4bd1e5f0feb8f310be8d6643684178
Shaikh Asef <asef04648@gmail.com>
2025-10-24 18:36:20 +0530

    upaded win posibilities

--- game.js
+++ game.js
@@ -46,27 +46,27 @@ function win(){
   if((arr[0]==1 && arr[1]==1 && arr[2]==1) || (arr[0]==2 && arr[1]==2 && arr[2]==2)){
     return true;
   }
-  // else if((arr[3] && arr[4] && arr[5]) || (!arr[3] && !arr[4] && !arr[5])){
-  //   return true;
-  // }
-  // else if((arr[6] && arr[7] && arr[8]) || (!arr[6] && !arr[7] && !arr[8])){
-  //   return true;
-  // }
-  // else if((arr[0] && arr[3] && arr[6]) || (!arr[0] && !!arr[3] && !arr[6])){
-  //   return true;
-  // }
-  // else if((arr[1] && arr[4] && arr[7]) || (!arr[1] && !arr[4] && !arr[7])){
-  //   return true;
-  // }
-  // else if((arr[2] && arr[5] && arr[8]) || (!arr[2] && !arr[5] && !arr[8])){
-  //   return true;
-  // }
-  // else if((arr[0] && arr[4] && arr[8]) || (!arr[0] && !arr[4] && !arr[8])){
-  //   return true;
-  // }
-  // else if((arr[2] && arr[4] && arr[6]) || (!arr[2] && !arr[4] && !arr[6])){
-  //   return true;
-  // }
+  else if((arr[3]==1 && arr[4]==1 && arr[5]==1) || (arr[3]==2 && arr[4]==2 && arr[5]==2)){
+    return true;
+  }
+  else if((arr[6]==1 && arr[7]==1 && arr[8]==1) || (arr[6]==2 && arr[7]==2 && arr[8]==2)){
+    return true;
+  }
+  else if((arr[0]==1 && arr[3]==1 && arr[6]==1) || (arr[0]==2 && arr[3]==2 && arr[6]==2)){
+    return true;
+  }
+  else if((arr[1]==1 && arr[4]==1 && arr[7]==1) || (arr[1]==2 && arr[4]==2 && arr[7]==2)){
+    return true;
+  }
+  else if((arr[2]==1 && arr[5]==1 && arr[8]==1) || (arr[2]==2 && arr[5]==2 && arr[8]==2)){
+    return true;
+  }
+  else if((arr[0]==1 && arr[4]==1 && arr[8]==1) || (arr[0]==2 && arr[4]==2 && arr[8]==2)){
+    return true;
+  }
+  else if((arr[2]==1 && arr[4]==1 && arr[6]==1) || (arr[2]==2 && arr[4]==2 && arr[6]==2)){
+    return true;
+  }
   return false;
 }
-restartBtn.addEventListener("click", function(){ alert("Hello World!"); });
+restartBtn.addEventListener("click", function(){ alert("GAME WILL BE RESATART !!!"); });
